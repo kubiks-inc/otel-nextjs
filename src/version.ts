678f64c3c5a2b603ec33a8ebb2e715b552ae8474
Alex Holovach <alex.holovach@kubiks.ai>
2025-08-15 18:55:24 -0500

    bump version

--- src/version.ts
+++ src/version.ts
@@ -3,7 +3,7 @@
  * This is automatically updated during the release process
  * @constant
  */
-export const PACKAGE_VERSION = '1.0.26';
+export const PACKAGE_VERSION = '1.0.27';
 
 /**
  * Gets the current package version
